41e0e23950d9edfd92ca498040a20368712182a5
umnovjp <umnovjp@yahoo.com>
2025-03-29 21:33:36 -0500

    STL win over COL

--- public/scripts/feedback.js
+++ public/scripts/feedback.js
@@ -1,6 +1,5 @@
 const feedbackForm = document.getElementById('feedback-form'); // this form is not used for now
 
-
 // Handle when a user submits feedback
 feedbackForm
   .addEventListener('submit', (e) => {
